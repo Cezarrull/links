aa79094f86301bd5a86228d03b795d708da7a6a6
Cézar Rull. <cezar@sophus.lan>
2024-10-28 15:56:27 -0300

    :tada: first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './global.css'
+import { PageOne } from './Pages/PageOne/PageOne'
+
+export function App() {
+  return (
+    <>
+      <PageOne />
+    </>
+  )
+}
---
+++ src/Pages/PageOne/PageOne.jsx
@@ -0,0 +1,11 @@
+import style from './PageOne.module.css'
+
+export function PageOne() {
+    return(
+        <>
+            <div className={style.pageBody}>
+                
+            </div>
+        </>
+    )
+}
