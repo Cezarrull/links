6ed611cb4c2e058d7b4c283f5e7d642da18c9955
Cezarrull <cezarrull12345@gmail.com>
2024-11-28 20:21:43 +0000

    :heavy_plus_sign: add new site (Twoone)

--- src/Pages/pageProjects/PageProjects.jsx
+++ src/Pages/pageProjects/PageProjects.jsx
@@ -17,6 +17,9 @@ export function PageProjects() {
                             <a href="https://www.rotarypovoador.org.br/" target='_blanck'>RotaryPovoador</a>
                         </li>
                         <li>
+                            <a href="https://twoone.netlify.app/" target='_blanck'>Twoone (em desenvolvimento)</a>
+                        </li>
+                        <li>
                             <a href="https://github.com/Cezarrull" target='_blanck'><GithubLogo size={20} />GitHub</a>
                         </li>
                     </ul>
