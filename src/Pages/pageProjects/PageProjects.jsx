bd8b4db5d1e7df9082dcffb4a5417f101bc2021b
Cezarrull <cezarrull12345@gmail.com>
2024-12-20 19:42:21 +0000

    :heavy_plus_sing: add new link site TWOONE

--- src/Pages/pageProjects/PageProjects.jsx
+++ src/Pages/pageProjects/PageProjects.jsx
@@ -14,6 +14,9 @@ export function PageProjects() {
                             <a href="https://allcarinfo.netlify.app/" target='_blanck'>AllCar</a>
                         </li>
                         <li>
+                            <a href="https://twoone.netlify.app/" target='_blanck'>TWOONE (Versão 1.0)</a>
+                        </li>
+                        <li>
                             <a href="https://www.rotarypovoador.org.br/" target='_blanck'>RotaryPovoador</a>
                         </li>
                         <li>
